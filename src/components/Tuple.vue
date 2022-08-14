556b7c99bb415d29ee90f3f6c83b2f86fe7d5cbc
James Johnson <johnsojr@umn.edu>
2022-08-13 21:54:54 -0500

    add inline variant

--- src/components/Tuple.vue
+++ src/components/Tuple.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="tuple mb-6 mt-4 w-full">
+  <div
+    class="tuple w-full"
+    :class="{
+      'grid grid-cols-3 justify-start items-baseline': variant === 'inline',
+    }"
+  >
     <span
       class="tuple__label text-xs block uppercase text-neutral-500 leading-none mb-1"
       >{{ label }}</span
     >
-    <span class="tuple__value block" v-bind="$attrs">
+    <span
+      class="tuple__value block"
+      :class="{
+        'col-span-2': variant === 'inline',
+      }"
+      v-bind="$attrs"
+    >
       <slot> - </slot>
     </span>
   </div>
 </template>
 <script setup lang="ts">
-defineProps<{
-  label: string;
-}>();
+withDefaults(
+  defineProps<{
+    label: string;
+    variant?: "stacked" | "inline";
+  }>(),
+  {
+    variant: "stacked",
+  }
+);
 </script>
 <script lang="ts">
 // use normal <script> to declare options
